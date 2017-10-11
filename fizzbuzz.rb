f8bff5589c8b0947c48663932098de7b07661d77
Larissa S <larissafschiavo@gmail.com>
2017-10-11 16:55:29 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(num)
-  if num % 15 == 0 
+  if num % 15 == 0
     return "FizzBuzz"
-  elsif num % 5 == 0 
+  elsif num % 5 == 0
     return "Buzz"
-  elsif num % 3 == 0 
+  elsif num % 3 == 0
     return "Fizz"
-  else 
-    return nil 
+  else
+    return nil
   end 
-end 
+end
